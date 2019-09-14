4b06934a7eacb3a6894140349421694af52faf8c
maddiestrange <maddie.strange@gmail.com>
2019-09-13 22:11:20 -0500

    answered questions

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
 import App from './App';
+import { isMainThread } from 'worker_threads';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+test(" app renders without crashing", () => {
+  render(<App />) });
+
+describe("cards class"), () => {
+  it('exists', () => {
+    const element = document.getElementById('cards');
+    expect(element).toBeDefined();
+  })
+}
+

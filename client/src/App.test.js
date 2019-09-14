23e2af73e3a520edcba630c4108434417b1bb893
maddiestrange <maddie.strange@gmail.com>
2019-09-13 22:48:11 -0500

    added another test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import * as rtl from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import App from './App'
 
 afterEach(rtl.cleanup);
 
@@ -17,5 +18,14 @@ it('renders without crashing', () => {
   expect(element).toBeVisible(); // jest-dom matcher
 });
 
+describe('PlayerCards', () => {
+  it('has an element with the text Blue', () =>{
+    const wrapper = rtl.render(<App />);
+
+    const button = wrapper.queryByText(/Blue!/i);
+    
+    expect(button).toBeInTheDocument();
+  })
+})
 
 
